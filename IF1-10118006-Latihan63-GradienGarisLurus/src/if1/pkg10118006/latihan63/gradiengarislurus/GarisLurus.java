9b927bb11010c3b6e8c5c0d0d12cfc4b62d2d0a0
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-10-24 19:57:20 +0700

    Add files via upload

---
+++ IF1-10118006-Latihan63-GradienGarisLurus/src/if1/pkg10118006/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,13 @@
+
+package if1.pkg10118006.latihan63.gradiengarislurus;
+
+/*
+ * NAMA              : Alfarizi Rizki Pane
+ * KELAS             : IF-01
+ * NIM               : 10118006
+ */
+public interface GarisLurus {
+    
+    public int hitungGradien();
+    
+}
